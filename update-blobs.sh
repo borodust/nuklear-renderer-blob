c804aceced7f72c6ce8146ac4fe67e824e9bd673
Pavel Korolev <dev@borodust.org>
2019-10-12 18:05:34 +0300

    Use new CI pipeline

--- update-blobs.sh
+++ update-blobs.sh
@@ -1,32 +1,6 @@
 #!/usr/bin/env bash
 
-name=nkrenderer
-repository_url=https://github.com/borodust/bodge-nuklear-renderer
-
-
-if [[ -z  $1 ]] ; then
-   echo "Version is not provided!"
-   exit 1;
-fi
-
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-declare -A platforms
-platforms=(["linux"]="so" ["osx"]="dylib" ["windows"]="dll")
-
-declare -A archs
-archs=(["x86_64"]="x86_64" ["i686"]="x86")
-
-for arch in "${!archs[@]}" ; do
-    target_dir="$script_dir/${archs[$arch]}"
-    mkdir -p "$target_dir"
-    for platform in "${!platforms[@]}" ; do
-        lib="lib$name.${platforms[$platform]}"
-        url="$repository_url/releases/download/$1/$lib-$arch-$platform-$1"
-        echo "Downloading $lib from $url"
-        if ! wget -O "$target_dir/$lib" -q "$url" ; then
-            echo "$lib download failed"
-            exit 1;
-        fi
-    done
-done
+lisp download-blobs nkrenderer https://github.com/borodust/bodge-nuklear-renderer \
+     "$script_dir/" $1
